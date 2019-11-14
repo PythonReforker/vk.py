cce2887a5df9a64f80088bb3cbb21456e582e50b
prostomarkeloff <botyavs@gmail.com>
2019-11-14 21:02:31 +0300

    some additions for #31

--- vk/utils/mixins.py
+++ vk/utils/mixins.py
@@ -2,8 +2,13 @@
 Author: https://github.com/aiogram/aiogram/blob/dev-2.x/aiogram/utils/mixins.py
 """
 import contextvars
+from enum import Enum
 from typing import Type
 from typing import TypeVar
+from typing import Union
+
+from pydantic import BaseModel
+from pydantic import validator
 
 T = TypeVar("T")
 
@@ -30,17 +35,78 @@ class ContextInstanceMixin:
         cls.__context_instance.set(value)
 
 
+class _MetaObjectType(Enum):
+    HANDLER = "handler"
+    MIDDLEWARE = "middleware"
+    BLUEPRINT = "blueprint"
+    EXTENSION = "extension"
+    RULE = "rule"
+
+    UNKNOWN = "unknown"
+
+
+class _MetaModel(BaseModel):
+    name: str = str
+    description: str = str
+    deprecated: bool = str
+    type: _MetaObjectType = _MetaObjectType.UNKNOWN
+
+    class Config:
+        extra = "allow"
+
+    @validator("type", pre=True)
+    def validate_type(cls, v: Union[_MetaObjectType, str]):
+        if isinstance(v, _MetaObjectType):
+            return v
+        elif isinstance(v, str):
+            try:
+                as_enum_value = _MetaObjectType(v)
+            except ValueError:
+                as_enum_value = _MetaObjectType.UNKNOWN
+            return as_enum_value
+        else:
+            raise TypeError("Unallowed type for field")
+
+    def get(self, key, default=None):
+        """
+        Be like `dict`.
+        :param key:
+        :param default:
+        :return:
+        """
+        return getattr(self, key, default)
+
+    def __str__(self):
+        args = ", ".join(
+            [f"{key}={value}" for key, value in self.dict().items()]
+        )
+        return "{}({})".format(self.__class__.__name__, args)
+
+
 class MetaMixin:
 
     # default meta tags:
     # name: str
     # description: str
     # deprecated: bool
+    # type: _MetaObjectType or str
 
     # meta = {
     # "name": "My object which contain meta variable",
     # "description": "Oh... i don't know..",
     # "deprecated": False,
     # }
+    # it's will be converted to `_MetaModel` with `dot-access` to attributes.
 
-    meta: dict = {}  # information about object special for third-party-addons.
+    meta: Union[
+        dict, _MetaModel
+    ] = {}  # information about object special for third-party-addons.
+
+    def __init_subclass__(cls, **kwargs):
+        if isinstance(cls.meta, dict):
+            new_meta = _MetaModel(**cls.meta)
+        elif isinstance(cls.meta, _MetaModel):
+            new_meta = cls.meta
+        else:
+            raise TypeError("Unallowed type for meta property")
+        cls.meta = new_meta
