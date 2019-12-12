9c427f54c92c05852c29f80f7b6a62fa11840f73
prostomarkeloff <somefuckingidiotfrominternet@pm.me>
2019-12-12 18:25:08 +0300

    fix tests for #37

--- tests/test_vk.py
+++ tests/test_vk.py
@@ -11,5 +11,4 @@ class TestVK:
         assert isinstance(vk.client, aiohttp.ClientSession)
         assert isinstance(vk.error_dispatcher, APIErrorDispatcher)
         assert vk.client.closed is False
-        assert VK.get_current() is not None
-        assert VK.get_current() is vk
+        assert VK.get_current() is None
